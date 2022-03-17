51b69d42f01381752f4983447c8a38a5fc97ce31
Marcell Endrey <marcell.endrey@prezi.com>
2022-03-17 06:06:25 +0100

    let it be so

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count = 0
+
+  function onClick() {
+    count++
+  }
+</script>
+
+<button on:click={onClick}>Click me!</button>
+<main>The button has been clicked {count} times.</main>
---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_FIREBASE_PROJECT_ID: string
+  VITE_FIREBASE_APP_ID: string
+  VITE_FIREBASE_MESSAGING_SENDER_ID: string
+  VITE_FIREBASE_API_KEY: string
+}
